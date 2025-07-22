b9c58120bfd27ddb4bfbdde1bef8509ce43a00e9
Santhoshkumar <santhoshsk7077@gmail.com>
2025-07-22 12:19:54 +0530

    Time: 0 ms (100%), Space: 44.1 MB (96.53%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,7 +3,7 @@ class Solution {
         int sum=0;
         int length = mat.length;
         for(int i =0;i<length;i++){
-            sum+=mati][i];
+            sum+=mat[i][i];
             sum+=mat[i][length-1-i];
         }
         if(length%2 == 1){
