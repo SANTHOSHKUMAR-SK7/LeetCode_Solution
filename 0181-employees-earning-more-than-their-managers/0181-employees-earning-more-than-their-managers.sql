8d6928222d5c48a6491b280eccf5976490c127db
Santhoshkumar <santhoshsk7077@gmail.com>
2025-08-02 09:53:05 +0530

    Update 0181-employees-earning-more-than-their-managers.sql

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,12 +1,4 @@
 # Write your MySQL query statement below
--- SELECT name as Employee
--- FROM Employee where managerId is not null
--- limit 1;
--- SELECT e1.name AS Employee
--- FROM Employee e1
--- JOIN Employee e2 ON e1.managerId = e2.id
--- WHERE e1.salary > e2.salary;
-
 SELECT emp1.
 name AS Employee FROM Employee emp1
 JOIN Employee emp2 ON emp1.managerId = emp2.id
